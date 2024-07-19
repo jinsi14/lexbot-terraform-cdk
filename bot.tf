38a58cd0afa967f0a21661ff9008e57a9dc32f8f
jinsi <>
2024-07-19 11:45:51 +0530

    update bot

--- bot.tf
+++ bot.tf
@@ -1,73 +1,77 @@
-# Create Lex V2 Bot
-resource "aws_lexv2models_bot" "example" {
-  name = "digiexamplelexv2"
-  data_privacy {
+
+//create s3 bucket
+resource "aws_s3_bucket" "lex_bot_bucket" {
+  bucket_prefix = "my-lex-bot-s3-bucket-${var.prefix_region}-${var.env}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "lex_bot_bucket" {
+  bucket = aws_s3_bucket.lex_bot_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+//upload bot zip file in s3 bucket
+resource "aws_s3_object" "lex_bot_zip" {
+  bucket = aws_s3_bucket.lex_bot_bucket.id
+  key    = "lex-bots/digiflowersv2-DRAFT-B56L841Z2E-LexJson.zip"
+  source = "./config/digiflowersv2-DRAFT-B56L841Z2E-LexJson.zip"
+  etag   = filemd5("./config/digiflowersv2-DRAFT-B56L841Z2E-LexJson.zip")
+}
+
+//create bot
+resource "awscc_lex_bot" "my_lex_bot" {
+  name                   = "my-lex-bot"
+  auto_build_bot_locales = true
+  role_arn               = aws_iam_role.lex_execution_role.arn
+  data_privacy = {
     child_directed = false
   }
   idle_session_ttl_in_seconds = 300
-  role_arn = aws_iam_role.lex_execution_role.arn
-
-  tags = {
-    created_by = "terraform"
+  bot_file_s3_location = {
+    s3_bucket     = aws_s3_bucket.lex_bot_bucket.id
+    s3_object_key = aws_s3_object.lex_bot_zip.key
   }
-}
-
-# Define Bot Locale ("en_US" in this example)
-resource "aws_lexv2models_bot_locale" "example" {
-  bot_id                           = aws_lexv2models_bot.example.id
-  bot_version                      = "DRAFT"
-  locale_id                        = "en_US"
-  n_lu_intent_confidence_threshold = 0.40
-
-  voice_settings {
-    voice_id = "Kendra"
-    engine   = "standard"
-  }
-}
-
-# Create a new version for the bot
-resource "aws_lexv2models_bot_version" "test" {
-  bot_id = aws_lexv2models_bot.example.id
-  locale_specification = {
-    "en_US" = {
-      source_bot_version = "DRAFT"
+  bot_tags = [
+    {
+      key   = "environment"
+      value = "${var.env}"
     }
-  }
+  ]
 }
 
-# Define the Intent
-resource "aws_lexv2models_intent" "example" {
-  bot_id      = aws_lexv2models_bot.example.id
-  bot_version = aws_lexv2models_bot_locale.example.bot_version
-  name        = "botens_namn"
-  locale_id   = aws_lexv2models_bot_locale.example.locale_id
-}
 
-# Create the Slot
-resource "aws_lexv2models_slot" "example" {
-   bot_id      = aws_lexv2models_bot.example.id
-  bot_version = aws_lexv2models_bot_version.test.bot_version
-  intent_id   = aws_lexv2models_intent.example.id
-  locale_id   = aws_lexv2models_bot_locale.example.locale_id
-  name          = "FlowerTypes"
+//create bot version
 
-  value_elicitation_setting {
-    slot_constraint = "Required"
-    prompt_specification {
-      message_group {
-        message {
-          plain_text_message {
-            value = "What type of flowers would you like to order?"
-          }
-        }
+resource "awscc_lex_bot_version" "my_lex_bot_version" {
+  bot_id     = awscc_lex_bot.my_lex_bot.id
+  bot_version_locale_specification = [
+    {
+      locale_id = "en_US"
+      bot_version_locale_details = {
+        source_bot_version = "DRAFT"
       }
-      max_retries = 2
-      allow_interrupt = true
     }
-  }
+  ]
 }
 
-  #confirmation_setting  {
-  #  data.local_file.order_flowers_intent_json
-  #} jsondecode(data.local_file.order_flowers_intent_json.content)
-#}
+
+//create lex bot alias
+resource "awscc_lex_bot_alias" "my_lex_bot_alias" {
+  bot_alias_name = "live"
+  bot_id         = awscc_lex_bot.my_lex_bot.id
+  bot_version    = awscc_lex_bot_version.my_lex_bot_version.bot_version
+  sentiment_analysis_settings = {
+    detect_sentiment = false
+  }
+  bot_alias_locale_settings = [
+    {
+      locale_id = "en_US"
+      bot_alias_locale_setting = {
+        enabled = true
+        code_hook_specification = null
+      }
+    }
+  ]
+}
--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lex_execution_role" {
-  name = "botens_namn"
+  name = "my-lex-bot-role-${var.prefix_region}-${var.env}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
