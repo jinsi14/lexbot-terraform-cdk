7190d98b5a2c0f3aae9c7db6f6b18960114b0864
jinsi <>
2024-07-25 16:20:35 +0530

    intial commit

--- aws_terraform_cdk/s3_bucket_stack.py
+++ aws_terraform_cdk/s3_bucket_stack.py
@@ -2,18 +2,14 @@ import aws_cdk as cdk
 from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
-class CdkS3Stack(cdk.Stack):
 
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-
-        # Create an S3 bucket
-        bucket = s3.Bucket(
-            self,
-            "aws-cdk-bucket-jinsi",
-            versioned=True,
-            removal_policy=cdk.RemovalPolicy.DESTROY,
-            auto_delete_objects=True
-        )
-
-        cdk.CfnOutput(self, "BucketName", value=bucket.bucket_name)
+def s3_bucket(self, scope: Construct, id: str, **kwargs) -> None:
+    bucket = s3.Bucket(
+        self,
+        id = "aws-cdk-test-bucket",
+        bucket_name="aws-cdk-bucket-jinsi",
+        versioned=True,
+        removal_policy=cdk.RemovalPolicy.DESTROY,
+        auto_delete_objects=True
+    )
+    cdk.CfnOutput(self, "BucketName", value=bucket.bucket_name)
--- tests/unit/test_lexbot_terraform_cdk_stack.py
+++ tests/unit/test_lexbot_terraform_cdk_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from aws_terraform_cdk.s3_bucket_stack import CdkS3Stack
+from aws_terraform_cdk.aws_terraform_cdk_stack import AwsTerraformCdkStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in lexbot_terraform_cdk/lexbot_terraform_cdk_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = CdkS3Stack(app, "aws-cdk-stack")
+    stack = AwsTerraformCdkStack(app, "Awds-terraform-cdk")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
